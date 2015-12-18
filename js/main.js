55f21f5c4aef4862cbe1b75fc652016dd5230e6d
Jacob Messina <jmessina@albany.edu>
2015-12-17 20:08:08 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $("#panel1").click(function(){
   $("#panelC1").toggle(1000);
 });
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "<iframe width="560" height="315" src="https://www.youtube.com/embed/UwGypUTEP4M" frameborder="0" allowfullscreen></iframe>";
+}
+</script>
